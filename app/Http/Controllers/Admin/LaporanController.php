5ca5c6a0e54d188e421889d6d0f3aabb15b0297d
jennn1-jr <jenaraditya123@gmail.com>
2026-05-07 19:15:55 +0700

    update laporan controller

--- app/Http/Controllers/Admin/LaporanController.php
+++ app/Http/Controllers/Admin/LaporanController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\HasilSaw;
-use App\Models\Pendaftar;
 use App\Models\PengaturanSistem;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class LaporanController extends Controller
 {
@@ -31,7 +31,17 @@ class LaporanController extends Controller
 
     public function exportPdf()
     {
-        // Akan diimplementasikan dengan dompdf
-        return back()->with('info', 'Fitur export PDF segera hadir.');
+        $kuota        = (int) PengaturanSistem::get('kuota_beasiswa', 5);
+        $periodeAktif = PengaturanSistem::get('periode_aktif', date('Y'));
+        $namaBeasiswa = PengaturanSistem::get('nama_beasiswa', 'Beasiswa PPA');
+        $hasil        = HasilSaw::with('pendaftar')->orderBy('peringkat')->get();
+
+        $pdf = Pdf::loadView('admin.laporan.cetak', compact('hasil', 'kuota', 'periodeAktif', 'namaBeasiswa'))
+                  ->setPaper('a4', 'portrait');
+
+        $filename = 'laporan-seleksi-' . strtolower(str_replace(' ', '-', $namaBeasiswa)) . '-' . $periodeAktif . '.pdf';
+
+        return $pdf->download($filename);
     }
 }
+
